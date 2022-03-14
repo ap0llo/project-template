692f1fe20334904d8402278bfca10551da5f7a2a
Andreas Grünwald <andreas@grynwald.net>
2022-03-14 11:22:39 +0100

    ci: Make .NET SDK installation collapsible in the Azure Pipelines log
    Emit group start/end log messages when running in Azure Pipelines to make the log of the .NET SDK installation collapsible

--- build.ps1
+++ build.ps1
@@ -1,10 +1,17 @@
 $ErrorActionPreference = "Stop"
 
+if ($env:TF_BUILD) {
+    Write-Host "##[group]Install .NET SDK"
+}
 # Install .NET 5 runtime (requried for runnign some of the local tools during build)
 ./build/dotnet-install.ps1 -Channel 5.0 -Runtime dotnet
 
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
+
+if ($env:TF_BUILD) {
+    Write-Host "##[endgroup]"
+}
 
 dotnet run --project build/Build.csproj -- $args
 exit $LASTEXITCODE
