535d227239b8263c8ff4a291d55373ce048ecd73
Andreas Grünwald <andreas@grynwald.net>
2022-03-14 11:23:27 +0100

    ci: List all .NET SDKs and runtime before running build

--- build.ps1
+++ build.ps1
@@ -3,11 +3,13 @@ $ErrorActionPreference = "Stop"
 if ($env:TF_BUILD) {
     Write-Host "##[group]Install .NET SDK"
 }
-# Install .NET 5 runtime (requried for runnign some of the local tools during build)
+# Install .NET 5 runtime (requried for running some of the local tools during build)
 ./build/dotnet-install.ps1 -Channel 5.0 -Runtime dotnet
 
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
+
+Invoke-Expression "dotnet --info"
 
 if ($env:TF_BUILD) {
     Write-Host "##[endgroup]"
